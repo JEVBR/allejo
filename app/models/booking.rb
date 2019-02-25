ff47b97611ee45468426944974df945f0892648c
Marcelo Miyachi <marcelo.m1811@gmail.com>
2019-02-25 18:52:00 -0300

    booking validations added

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,8 @@
 class Booking < ApplicationRecord
   belongs_to :pitch
   belongs_to :user
+
+  validates :start_time, presence: true
+  validates :end_time, presence: true
+  validates :date, presence: true
 end
