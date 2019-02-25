a8465cf64d483e544377a10c96b084f8c3e68702
Marcelo Miyachi <marcelo.m1811@gmail.com>
2019-02-25 18:46:52 -0300

    pitch validations added

--- app/models/pitch.rb
+++ app/models/pitch.rb
@@ -2,5 +2,13 @@ class Pitch < ApplicationRecord
   belongs_to :user
   belongs_to :category
 
-  has_many :daily_schedules
+  has_many :daily_schedules, dependent: :destroy
+
+  validates :title, presence: true
+  validates :subtitle, presence: true
+
+  validates :address, presence: true
+  validates :cep, presence: true
+  validates :cnpj, presence: true
+  validates :price, presence: true
 end
