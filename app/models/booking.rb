e7ee74899f2154e7ba284091685718aac9bbbfb8
Marcelo Miyachi <marcelo.m1811@gmail.com>
2019-02-26 16:26:23 -0300

    booking validations added

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -10,6 +10,7 @@ class Booking < ApplicationRecord
 
   validate :check_end_time_greater_start_time
   validate :check_availability
+  validate :check_start_time_equal_end_time
 
   def check_end_time_greater_start_time
     if start_time > end_time
@@ -18,6 +19,22 @@ class Booking < ApplicationRecord
   end
 
   def check_availability
-    raise
+    new_booking_start_time = start_time
+    new_booking_end_time = end_time
+
+    if pitch.bookings.where(
+      "? < start_time AND ? > end_time", new_booking_start_time, new_booking_end_time).size.positive?
+      errors.add(:start_time, "there is a game in this time")
+
+    elsif pitch.bookings.where(
+      "? > start_time AND ? < end_time", new_booking_end_time, new_booking_start_time).size.positive?
+      errors.add(:end_time, "there is a game in this time")
+    end
+  end
+
+  def check_start_time_equal_end_time
+    if start_time == end_time
+      errors.add(:start_time, "start_time can't be equal end_time")
+    end
   end
 end
