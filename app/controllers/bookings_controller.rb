95fd9415caecbac63ec53ff6d91df5aebeb732fa
Marcelo Miyachi <marcelo.m1811@gmail.com>
2019-02-26 14:26:56 -0300

    start_time and end_time methods added to booking controller

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,10 +1,31 @@
 class BookingsController < ApplicationController
 
+  def start_time
+    start_year = booking_params["start_time(1i)"].to_i
+    start_month = booking_params["start_time(2i)"].to_i
+    start_day = booking_params["start_time(3i)"].to_i
+    start_hour = booking_params["start_time(4i)"].to_i
+    start_minute = booking_params["start_time(5i)"].to_i
+    DateTime.new(start_year, start_month, start_day, start_hour, start_minute)
+  end
+
+  def end_time
+    end_year = booking_params["end_time(1i)"].to_i
+    end_month = booking_params["end_time(2i)"].to_i
+    end_day = booking_params["end_time(3i)"].to_i
+    end_hour = booking_params["end_time(4i)"].to_i
+    end_minute = booking_params["end_time(5i)"].to_i
+    DateTime.new(end_year, end_month, end_day, end_hour, end_minute)
+  end
+
   def create
     @booking = Booking.new
     authorize @booking
     @booking.pitch_id = params[:pitch_id]
     @booking.user_id = current_user.id
+
+    @booking.start_time = start_time
+    @booking.end_time = end_time
   end
 
   private
