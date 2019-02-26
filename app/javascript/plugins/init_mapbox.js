1b21df1da2ab5796f0e32b2bdcf4c08c648d2fcd
Jeroen Haisma <J_Haisma@hotmail.com>
2019-02-26 20:03:17 -0300

    marcador_vermelho_na_mapa

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -21,8 +21,38 @@ const addPopUps = (marker) => {
                               <p>${marker.pitch_address}</p>
                               <img src="${marker.pitch_photo}" alt="No photos" style="width:200px;height:200px;">
                             </a>`);
+
   return popup;
 };
+
+const addWhereAmI = (marker) => {
+  const popup = new mapboxgl.Popup()
+                  .setHTML(`<a href ="${marker.pitch_link}">
+                            <h6>Voce esta aqui</h6>
+                            </a>`);
+  return popup;
+};
+
+const addCircleToMarker =(map, marker) => {
+  console.log(map);
+  console.log(marker);
+
+  map.addSource("polygon", createGeoJSONCircle([marker.lng, marker.lat], 10,20));
+
+map.addLayer({
+    "id": "polygon",
+    "type": "fill",
+    "source": "polygon",
+    "layout": {
+'visibility': 'visible'
+},
+    "paint": {
+        "fill-color": "blue",
+        "fill-opacity": 0.6
+    }
+});
+
+}
 
 const addMarkersToMap = (map, markers) => {
   markers.forEach((marker) => {
@@ -35,13 +65,18 @@ const addMarkersToMap = (map, markers) => {
         .addTo(map);
       map.flyTo({ center: [ marker.lng, marker.lat ] });
     } else{
+      const popup = addWhereAmI(marker)
       new mapboxgl.Marker({color: 'red'})
         .setLngLat([ marker.lng, marker.lat ])
+        .setPopup(popup)
         .addTo(map);
+      addCircleToMarker(map,marker);
       map.flyTo({ center: [ marker.lng, marker.lat ] });
     }
   });
 };
+
+
 
 const fitMapToMarkers = (map, markers) => {
   const bounds = new mapboxgl.LngLatBounds();
@@ -57,59 +92,47 @@ const initMapbox = () => {
     // initSearchForm(map);
     const markers = JSON.parse(mapElement.dataset.markers);
     addMarkersToMap(map, markers);
-
-    map.addLayer({
-      "id": "circle500",
-      "type": "circle",
-      "source": "source_circle_500",
-      "layout": {
-        "visibility": "none"
-      },
-      "paint": {
-        "circle-radius": 804672,
-        "circle-color": "#5b94c6",
-        "circle-opacity": 0.6
-      }
-    });
   }
+};
+
+var createGeoJSONCircle = function(center, radiusInKm, points) {
+    if(!points) points = 64;
+
+    var coords = {
+        latitude: center[1],
+        longitude: center[0]
+    };
+
+    var km = radiusInKm;
+
+    var ret = [];
+    var distanceX = km/(111.320*Math.cos(coords.latitude*Math.PI/180));
+    var distanceY = km/110.574;
+
+    var theta, x, y;
+    for(var i=0; i<points; i++) {
+        theta = (i/points)*(2*Math.PI);
+        x = distanceX*Math.cos(theta);
+        y = distanceY*Math.sin(theta);
+
+        ret.push([coords.longitude+x, coords.latitude+y]);
+    }
+    ret.push(ret[0]);
+    console.log(ret);
+    return {
+        "type": "geojson",
+        "data": {
+            "type": "FeatureCollection",
+            "features": [{
+                "type": "Feature",
+                "geometry": {
+                    "type": "Polygon",
+                    "coordinates": [ret]
+                }
+            }]
+        }
+    };
 };
 
 export { initMapbox };
 
-
-// import mapboxgl from 'mapbox-gl';
-
-// const mapElement = document.getElementById('map');
-
-// const buildMap = () => {
-//   mapboxgl.accessToken = mapElement.dataset.mapboxApiKey;
-//   return new mapboxgl.Map({
-//     container: 'map',
-//     style: 'mapbox://styles/mapbox/streets-v10'
-//   });
-// };
-
-// const addMarkersToMap = (map, markers) => {
-//   markers.forEach((marker) => {
-//     new mapboxgl.Marker()
-//       .setLngLat([ marker.lng, marker.lat ])
-//       .addTo(map);
-//   });
-// };
-
-// const fitMapToMarkers = (map, markers) => {
-//   const bounds = new mapboxgl.LngLatBounds();
-//   markers.forEach(marker => bounds.extend([ marker.lng, marker.lat ]));
-//   map.fitBounds(bounds, { padding: 70, maxZoom: 15 });
-// };
-
-// const initMapbox = () => {
-//   if (mapElement) {
-//     const map = buildMap();
-//     const markers = JSON.parse(mapElement.dataset.markers);
-//     addMarkersToMap(map, markers);
-//     fitMapToMarkers(map, markers);
-//   }
-// };
-
-// export { initMapbox };
