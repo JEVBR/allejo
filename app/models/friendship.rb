03385a4f586ba0ec071d29e46d4c0652f4b04f11
Marcelo Miyachi <marcelo.m1811@gmail.com>
2019-02-28 17:55:27 -0300

    add friends to both user

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -3,4 +3,11 @@ class Friendship < ApplicationRecord
   belongs_to :friend, :class_name => 'User'
 
   validates_uniqueness_of :friend_id, scope: :user_id
+  validate :check_friend_user
+
+  def check_friend_user
+    if user == friend
+      errors.add(:friend, "can't add yourself")
+    end
+  end
 end
