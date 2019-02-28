2407ab7ed383a0bde8023edc2d369923e55f5201
Marcelo Miyachi <marcelo.m1811@gmail.com>
2019-02-28 12:01:28 -0300

    validations added to PlayerList

--- app/models/player_list.rb
+++ app/models/player_list.rb
@@ -1,4 +1,7 @@
 class PlayerList < ApplicationRecord
   belongs_to :booking
   belongs_to :user
+
+  validates :booking_id, presence: true
+  validates_uniqueness_of :user_id, scope: :booking_id
 end
