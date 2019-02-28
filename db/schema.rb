bc31c112706078b0d8d40cfca9f33fc05688f89e
Alvaro Palermo <alvaro.palermo68@gmail.com>
2019-02-28 18:01:20 -0300

    Seeded Players and Pithces

--- db/schema.rb
+++ db/schema.rb
@@ -60,15 +60,6 @@ ActiveRecord::Schema.define(version: 2019_02_28_150451) do
     t.index ["user_id"], name: "index_pitches_on_user_id"
   end
 
-  create_table "player_lists", force: :cascade do |t|
-    t.bigint "booking_id"
-    t.bigint "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["booking_id"], name: "index_player_lists_on_booking_id"
-    t.index ["user_id"], name: "index_player_lists_on_user_id"
-  end
-
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -95,6 +86,4 @@ ActiveRecord::Schema.define(version: 2019_02_28_150451) do
   add_foreign_key "participants", "users"
   add_foreign_key "pitches", "categories"
   add_foreign_key "pitches", "users"
-  add_foreign_key "player_lists", "bookings"
-  add_foreign_key "player_lists", "users"
 end
