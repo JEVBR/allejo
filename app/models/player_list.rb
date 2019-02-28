9207e3680fc80b1fb7eac311ba1a1cd26a63d22e
Marcelo Miyachi <marcelo.m1811@gmail.com>
2019-02-28 12:03:54 -0300

    player_list model destroyed

--- app/models/player_list.rb
+++
@@ -1,7 +0,0 @@
-class PlayerList < ApplicationRecord
-  belongs_to :booking
-  belongs_to :user
-
-  validates :booking_id, presence: true
-  validates_uniqueness_of :user_id, scope: :booking_id
-end
--- test/models/player_list_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class PlayerListTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
