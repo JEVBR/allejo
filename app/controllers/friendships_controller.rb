8d3d9b8ebd8aae76604b389264c12aa9454b73a0
Marcelo Miyachi <marcelo.m1811@gmail.com>
2019-02-28 17:39:12 -0300

    add friends with email added to controller

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,6 +1,18 @@
 class FriendshipsController < ApplicationController
-
   def create
-    raise
+    friend = User.find_by(email: params[:email])
+    friendship = Friendship.new
+    authorize friendship
+
+    if friend.present?
+      friendship = Friendship.new(user: current_user, friend_id: friend.id)
+    end
+
+    if friendship.valid?
+      friendship.save
+      redirect_to request.env["HTTP_REFERER"], notice: "#{friend.first_name} #{friend.last_name} foi adicionado a sua lista de amigos"
+    else
+      redirect_to request.env["HTTP_REFERER"], alert: "#{params[:email]} não foi encontrado"
+    end
   end
 end
