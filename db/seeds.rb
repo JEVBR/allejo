624944da7426acc53f14c917787ed444460927ce
Marcelo Miyachi <marcelo.m1811@gmail.com>
2019-03-10 23:05:01 -0300

    change id of seeds categories

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,9 +7,9 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 puts "Seeding categories"
-Category.create(name: 'Futsal')
 Category.create(name: 'Society')
 Category.create(name: 'Campo')
+Category.create(name: 'Futsal')
 puts "Categories seeded"
 
 puts "Seeding positions(player)"
