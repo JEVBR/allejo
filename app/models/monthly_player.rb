f50bc84fd7f8d02d62eab5b5eb25a566730578f9
Marcelo Miyachi <marcelo.m1811@gmail.com>
2019-03-21 12:57:58 -0300

    monthly_player basic validations added

--- app/models/monthly_player.rb
+++ app/models/monthly_player.rb
@@ -1,3 +1,9 @@
 class MonthlyPlayer < ApplicationRecord
   belongs_to :pitch
+
+  validates :start_time, presence: true
+  validates :end_time, presence: true
+  validates :player_name, presence: true
+  validates :player_phone, presence: true
+  validates :player_email, presence: true
 end
