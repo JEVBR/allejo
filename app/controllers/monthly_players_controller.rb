8a80260ce6595a7324361672d0b8b77bcee7327f
Marcelo Miyachi <marcelo.m1811@gmail.com>
2019-03-22 10:11:39 -0300

    monthlyPlayer#index route added

--- app/controllers/monthly_players_controller.rb
+++ app/controllers/monthly_players_controller.rb
@@ -1,2 +1,4 @@
 class MonthlyPlayersController < ApplicationController
+  def index
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -18,6 +18,7 @@ Rails.application.routes.draw do
 
   resources :pitches do
     resources :bookings, only: [:create]
+    resources :monthly_players, only: [:index]
   end
 
 
