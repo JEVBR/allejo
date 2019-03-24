c6d3767e3a8c619d77d64338d9b6ab01c9eba305
Marcelo Miyachi <marcelo.m1811@gmail.com>
2019-03-24 11:34:00 -0300

    time_options added to monthly_player edit form

--- app/controllers/monthly_players_controller.rb
+++ app/controllers/monthly_players_controller.rb
@@ -34,28 +34,21 @@ class MonthlyPlayersController < ApplicationController
   end
 
   def update
-    pitch_id = @monthly_player.pitch.id
+    @pitch = @monthly_player.pitch
 
-    if @monthly_player.update(monthly_player_params.merge(pitch_id: pitch_id))
-      # new_monthly = MonthlyPlayer.new(monthly_player_params)
-      # new_monthly.pitch_id = @monthly_player.pitch_id
+    opening_time = (@pitch.opening_time.to_f / 60).ceil
+    closing_time = (@pitch.closing_time / 60).floor
 
-      # @monthly_player.destroy
+    @time_options = []
 
-      # if new_monthly.valid?
-      #   new_monthly.save
-      #   redirect_to pitch_monthly_players_path(new_monthly.pitch), notice: 'Mensalista criado com sucesso!'
-      # else
-      #   @monthly_player = MonthlyPlayer.create(@monthly_player.attributes)
-      #   render :new
-      # end
-    # monthly_player = MonthlyPlayer.new(monthly_player_params)
+    (opening_time..closing_time).step(0.5) do |time|
+      @time_options << ["#{time.floor}:#{format('%02d', ((time * 60) % 60))}", time * 60]
+    end
 
-    # if monthly_player.valid?
-    #   monthly_player.save
-      redirect_to pitch_monthly_players_path(pitch_id)
+    if @monthly_player.update(monthly_player_params.merge(pitch_id: @pitch.id))
+      redirect_to pitch_monthly_players_path(@pitch.id)
     else
-      raise
+      render :edit
     end
   end
 
